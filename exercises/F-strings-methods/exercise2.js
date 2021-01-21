b39950dfdbea269348965492fb486fc5a31dcc55
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-01-21 22:10:25 +0100

    Exercises A-G

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
 const name = " Daniel  ";
+var nameLength = name.length;
+console.log("My name is " + name.trim() + " and my name is " + nameLength + " characters long" );
+console.log(`My name is ${name.trim()} and my name is ${nameLength} characters long`);
 
-console.log(message);
